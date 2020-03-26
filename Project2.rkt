78a114f8c3942715ed422acdcb56dded03110b22
Mrigank Doshy <32623983+mrigank2487@users.noreply.github.com>
2020-03-26 17:42:28 -0400

    Add files via upload

--- Project2.rkt
+++ Project2.rkt
@@ -18,6 +18,41 @@
 (keep-if f '(10 1 7 2))
 
 
+; Question 3
+
+
+
+; Question 4
+(define (to-words n)
+(define OneToNineteen '(one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen))
+(define TenMultiples '(twenty thirty forty fifty sixty seventy eighty ninety))
+(define hundreds '(hundred ))
+
+(cond
+  ((= n 0) '(zero))
+  (else
+   (let loop ((n n) (units hundreds) (res '()))
+     (cond
+       
+       ((< n 0) (append '(negative) (to-words(abs n))))
+       
+       ((= n 0) res)
+       
+       ((< 0 n 20) (cons (list-ref OneToNineteen (- n 1)) res))
+       ((< n 100) (cons (list-ref TenMultiples (- (quotient n 10) 2))
+                        (loop (remainder n 10) '() res)))
+      
+       (else
+        '(error)))))))
+
+
+(to-words 13)
+(to-words -55)
+(to-words 0)
+(to-words 120)
+
+
+
 
   
 
